fb437362378ee4233706cfb6d81bcdf742b284b2
Lucas Menezes <lsm.developer1@gmail.com>
2026-01-30 23:46:33 -0300

    fix: correção cors3

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,12 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return cors(corsOptions)(req, res, () => res.sendStatus(204));
+  }
+  next();
+});
 
 app.use( express.json({
     verify: (req, res, buf) => {
