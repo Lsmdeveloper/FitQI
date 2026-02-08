db7efae18c277700d6993e83f46cbbc8b8c94f29
Lucas Menezes <lsm.developer1@gmail.com>
2026-02-08 20:59:26 -0300

    fix/ imag logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="w-full border-b bg-white">
       <div className="mx-auto flex max-w-3xl items-center justify-between px-4 py-3">
         <img
-            src="../public/blue-logo.png"
+            src="/blue-logo.png"
             alt="FitIQ"
             className="h-12 w-12"
         />
